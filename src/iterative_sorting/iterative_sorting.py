b4d6d388efe1a4b0fb5a3fe3af8934e56f1ce687
Modurotolu Olokode <durolawk@gmail.com>
2020-02-17 17:03:26 +0100

    implemented bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,24 +17,18 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    for (index, value) in enumerate(arr):
-        while arr[index] > arr[index + 1]:
-            print(index, 1)
-            if arr[index] > arr[index + 1]:
-                print(index, 2)
-                for i in range(0, len(arr) - 1):
-                    cur_index = i
-
-                    if arr[cur_index] > arr[cur_index + 1]:
-                        #print(index, value)
-                        #print(arr[cur_index], arr[cur_index + 1], 'b')
-                        arr[cur_index], arr[cur_index + 1] = arr[cur_index + 1], arr[cur_index]
-                        #print(arr[cur_index], arr[cur_index + 1], 'a')
+    sorted = False
+    while sorted == False:
+        sorted = True
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                sorted = False
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
 
     print(arr)
     return arr
 
-selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
+bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
 #bubble_sort([1, 5, 4, 2, 6, 0, 3, 7, 8, 9])
 
 #bubble_sort([1, 4, 2, 5, 6, 0, 3, 7, 8, 9])
